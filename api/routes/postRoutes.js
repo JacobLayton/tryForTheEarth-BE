9db2c3eb3fab1fbabc2cb3d7868d2bd55a7f90e7
Jacob Layton <jlayjlay3@gmail.com>
2021-02-26 22:40:14 -0800

    Post routes complete

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('../../data/dbConfig')
+const db = require('../../data/dbConfig');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 // GET all users
 router.get('/', async (req, res) => {
@@ -15,6 +18,7 @@ router.get('/', async (req, res) => {
     });
 });
 
+// GET post with id
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     await db('posts').where({ id })
@@ -29,15 +33,51 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ message: 'Failed to get post.'});
     });
 });
-  
-router.post('/', async (req, res) => {
+
+// POST new post
+router.post('/', jsonParser, async (req, res) => {
     await db('posts').insert(req.body)
     .then(post => {
         if(post) {
-            console.log(post);
             return res.status(200).json({ message: 'Post was created successfully.'});
         } else {
             return res.status(404).json({ error: 'Could not create new post' });
+        }
+    })
+    .catch(err => {
+        return res.status(500).json(err);
+    });
+});
+
+// PUT edit post
+router.put('/:id', jsonParser, async (req, res) => {
+    const { id } = req.params;
+    await db('posts').where({ id: id })
+    .update(req.body)
+    .then(updatedPost => {
+        if(updatedPost) {
+            return res.status(200).json({ message: 'Post was successfully updated.' });
+        } else {
+            return res.status(404).json({ error: 'Could not update post with that id.' });
+        }
+    })
+    .catch(err => {
+        return res.status(500).json(err);
+    });
+
+});
+
+// DELETE post at id
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await db('posts').where({ id: id })
+    .del()
+    .then(deletedPost => {
+        if(deletedPost) {
+            return res.status(200).json({ message: 'Post was successfully deleted.' });
+        } else  {
+            return res.status(404).json({ error: 'Could not delete post with that id.' });
+
         }
     })
     .catch(err => {
